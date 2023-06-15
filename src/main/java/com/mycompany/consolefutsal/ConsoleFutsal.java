629730ed38861ee9ba6c98da4f3e49e2cf4942f3
Saugatsaugat <saugatthapa2073@gmail.com>
2023-06-15 09:16:30 +0545

    Starting Menu with banner created

--- src/main/java/com/mycompany/consolefutsal/ConsoleFutsal.java
+++ src/main/java/com/mycompany/consolefutsal/ConsoleFutsal.java
@@ -1,6 +1,11 @@
 
 package com.mycompany.consolefutsal;
 
+import static java.lang.System.exit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author saugat
@@ -8,6 +13,27 @@ package com.mycompany.consolefutsal;
 public class ConsoleFutsal {
 
     public static void main(String[] args) {
-        
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("*******************************\n\n");
+        System.out.println("!!! WELCOME TO  FUTSAL !!!\n\n");
+        System.out.println("*******************************\n");
+        while(flag){
+        System.out.println("\nWhat do you want?\n1.Register\n2.Login\n3.Exit\nSelect: ");
+        try {
+            int ch = sc.nextInt();
+            switch (ch) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        }
     }
 }
